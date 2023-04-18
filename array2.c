ae45c1c30b925c738f4b9eb29d6a6c32efe800fb
Ceren Şahin <107798510+cerensahinnn@users.noreply.github.com>
2023-04-18 15:37:24 +0300

    Update array2.c

--- array2.c
+++ array2.c
@@ -1,28 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  const int SIZE = 5; // array boyutu
-  int myArray[SIZE]; // kullanıcıdan alınacak elemanların saklanacağı array
-  cout << "Lutfen " << SIZE << " adet tam sayi girin: " << endl;
+  int myArray[5] = {1, 2, 3, 4, 5};
 
-  // Kullanıcıdan elemanları alma
-  for(int i = 0; i < SIZE; i++) {
-    cout << "Eleman " << i + 1 << ": ";
-    cin >> myArray[i];
-  }
-
-  cout << "Girilen elemanlar: ";
-  // Array elemanlarını yazdırma
-  for(int i = 0; i < SIZE; i++) {
+  cout << "Array elemanlari: ";
+  for(int i = 0; i < 5; i++) {
     cout << myArray[i] << " ";
   }
   cout << endl;
 
   return 0;
 }
-
-// << ve >>: << ve >> (akış operatörleri), C++ dilinde çıktı ve girdi akışını temsil eder. 
-// << operatörü, cout nesnesine veriyi yazdırmak için kullanılırken, 
-// >> operatörü, cin nesnesinden veriyi okumak için kullanılır. 
-// << ve >> operatörleri, cout ve cin nesnelerine veri akışını sağlar ve verileri ekrana yazdırabilir 
-// veya kullanıcıdan girdi alabiliriz.
